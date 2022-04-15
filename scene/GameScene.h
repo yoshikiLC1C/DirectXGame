203bd53985c7e03a6c2908dcce091ef77d052bde
itouyoshiki <k021g1467@g.neec.ac.jp>
2022-04-15 14:36:02 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -47,6 +47,16 @@ class GameScene {
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
+	uint32_t textureHandle_ = 0;
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;
+	////サウンドデータハンドル
+	//uint32_t soundDataHandle_ = 0;
+	//uint32_t voiceHandle_ = 0;
+	// 値を表示したい変数
+	int32_t value_ = 0;
 
 	/// <summary>
 	/// ゲームシーン用
